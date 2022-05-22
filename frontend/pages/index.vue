90870be10f1a86f853a0821eefd7ef99e2950280
Antriko <creeperdrag772@gmail.com>
2022-05-22 17:29:29 +0100

    change

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,11 +1,13 @@
-<script>
+<script setup>
+const token = useCookie("token")
 </script>
 
 <template>
     <div>
         <div class="flex flex-wrap pa3 pa5-l">
             <div class="w-100 mt3 mb3 f2">
-                <NuxtLink class="link black dim" to="/booking">Book now</NuxtLink>
+                <NuxtLink class="link black dim" to="/booking" v-if="token">Book now</NuxtLink>
+                <NuxtLink class="link black dim" to="/user/login" v-else>Login</NuxtLink>
             </div>
             <div class="w-100 mt3 mb3 f5">
                 Created and designed by Antriko H.
