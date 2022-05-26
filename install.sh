b42bc54fa945af39ca226a662932144783e57476
Antriko <creeperdrag772@gmail.com>
2022-05-26 12:22:58 +0100

    docker

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ mkdir ./certs/conf
 mkdir logs
 touch logs/backend.txt
 docker-compose -f docker-compose.initial.yml build
-docker-compose -f docker-compose.initial.yml up -d nginx
+docker-compose -f docker-compose.initial.yml up -d assignment-nginx
 echo "🔍 Hit ctrl+d after you are done with certbot!"
-docker-compose exec nginx certbot certonly --nginx -d parking.antriko.co.uk
+docker-compose exec assignment-nginx certbot certonly --nginx -d parking.antriko.co.uk
 docker-compose down
 echo "🚀 Done!"
