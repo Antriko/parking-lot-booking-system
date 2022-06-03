c3af77e3f846a15cfd3b85bf1e115bb05ffe7616
Antriko <creeperdrag772@gmail.com>
2022-06-03 13:31:42 +0100

    db

--- backend/app.js
+++ backend/app.js
@@ -32,19 +32,24 @@ const db = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, proce
 
 
 (async () => {
+    await db.authenticate()
+        .then(() => console.log("Connected"))
+        .catch(e => console.log(e))
+
+        
     // Create database
     await db.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_DATABASE}`)
         .catch(e => console.log("caught", e))
 
 
     // Create all needed tables for app
-    await db.query("CREATE TABLE IF NOT EXISTS `user` (`ID` int(11) NOT NULL AUTO_INCREMENT, `username` varchar(16) NOT NULL, `password` varchar(16) NOT NULL, PRIMARY KEY (`ID`))")
+    await db.query("CREATE TABLE IF NOT EXISTS `parkinglot`.`user` (`ID` int(11) NOT NULL AUTO_INCREMENT, `username` varchar(16) NOT NULL, `password` varchar(16) NOT NULL, PRIMARY KEY (`ID`))")
         .catch(e => console.log("caught"))
 
     await db.query("CREATE TABLE IF NOT EXISTS `parkinglot`.`bookings` ( `ID` INT NOT NULL AUTO_INCREMENT , `UserID` INT NOT NULL , `timeFrom` DATETIME NOT NULL , `timeTo` DATETIME NOT NULL , `parkingSlot` INT NOT NULL , PRIMARY KEY (`ID`))")
         .catch(e => console.log("caught"))
 
-    await db.query("CREATE TABLE IF NOT EXISTS `vehicle` (`ID` int(11) NOT NULL AUTO_INCREMENT, `UserID` int(11) NOT NULL, `vehicleName` varchar(45) NOT NULL, `vehicleReg` varchar(45) NOT NULL, PRIMARY KEY (`ID`))")
+    await db.query("CREATE TABLE IF NOT EXISTS `parkinglot`.`vehicle` (`ID` int(11) NOT NULL AUTO_INCREMENT, `UserID` int(11) NOT NULL, `vehicleName` varchar(45) NOT NULL, `vehicleReg` varchar(45) NOT NULL, PRIMARY KEY (`ID`))")
         .catch(e => console.log(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, process.env.DB_HOST))
 })();
 
